64b3541a4753c351e3d95adb8ef94aff02e04f26
luciel53 <l.leroty@gmail.com>
2023-09-02 11:32:38 +0200

    added task 2 nested functions

---
+++ Javascript_advanced/1-nested_functions.js
@@ -0,0 +1,18 @@
+let globalVariable = 'Welcome'
+
+function outer () {
+	alert(globalVariable);
+	let course = "Holberton";
+
+	function inner () {
+		alert(globalVariable + ' ' + course);
+		let exclamation = "!";
+
+		function inception () {
+			alert(globalVariable + ' ' + course + exclamation);
+		};
+		inception();
+	};
+	inner();
+}
+outer ();
