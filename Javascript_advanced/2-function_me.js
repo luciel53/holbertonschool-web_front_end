26bb74ad1d5ba4369e57ee631661773fb3efdfcd
luciel53 <l.leroty@gmail.com>
2023-09-02 11:59:10 +0200

    add task 2 welcome message

---
+++ Javascript_advanced/2-function_me.js
@@ -0,0 +1,7 @@
+function welcomeMessage (fullName) {
+	alert("Welcome" + ' ' + fullName);
+}
+
+let guillaume = welcomeMessage('Guillaume');
+let alex = welcomeMessage('Alex');
+let fred = welcomeMessage('Fred');
