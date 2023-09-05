209ff3994f45b296905d10c9bebb7c8e60786144
luciel53 <l.leroty@gmail.com>
2023-09-05 18:33:13 +0200

    add task 11

---
+++ Javascript_advanced/11-prime.js
@@ -0,0 +1,33 @@
+let numberPrime = 0;
+
+/* function that checks if a number is prime */
+function primeNumber (number) {
+	if (number < 2) {
+		return (false);
+	}
+	/* browse the boucle till i reaches square root of number */
+	for (let i = 2; i <= Math.sqrt(number); i++) {
+		if (number % i === 0) {
+			return (false);
+		}
+	}
+	return (true);
+}
+
+/* Function that counts how many numbers are prime */
+function countPrimeNumbers() {
+	for (number = 2; number < 101; number++) {
+		if (primeNumber(number)) {
+			numberPrime++;
+		}
+	}
+}
+
+const perf1 = performance.now();
+for (let cent = 0; cent < 100; cent++) {
+	setTimeout(countPrimeNumbers, 0);
+}
+
+const perf2 = performance.now();
+console.log("Execution time of calculating prime numbers 100 times was " + (perf2 - perf1) + " milliseconds.");
+
